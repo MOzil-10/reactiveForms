460a302d19db22657fa78519413e4179119aeb0b
MOzil-10 <mukosibudeli@gmail.com>
2023-06-18 02:51:16 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+//firstly we declare the formGroup variable
+registerForm!:FormGroup  //this binds it to our angular component
+
   title = 'formValiadation';
+
+
+  submitted = false ;  //the form is not submitted when we load the page for the very first time
+
+  constructor(private formBuilder:FormBuilder){}
+
+//when this component is initialized this function will automaticall trigger
+    ngOnInit(){
+
+      //Validations
+
+      this.registerForm= this.formBuilder.group({
+        firstName:['',Validators.required], //if user submits nothing this validation will trigger
+        lastName:['',Validators.required],
+        email:['',[Validators.required,Validators.email]],
+        password:['',[Validators.required,Validators.minLength(6)]]
+      })
+    }
+
+    onSubmit(){
+this.submitted=true;
+
+if(this.registerForm.invalid){
+  return
 }
+
+//if all the fields are successfully filed then display this 
+alert("Success")
+    }
+  }
+
+
+
